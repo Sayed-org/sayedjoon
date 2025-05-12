9a2fd26a8da0ea113aa173c78b20197a22aee8e0
Sayedtaqi313 <141312215+Sayedtaqi313@users.noreply.github.com>
2025-05-12 08:36:14 -0700

    Delete frontend directory

--- frontend/src/app/Pages/Api/Handler.tsx
+++
@@ -1,8 +0,0 @@
-// frontend/pages/api/hello.js
-// pages/api/hello.ts
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: "Hello from Next.js!" });
-}
- 
